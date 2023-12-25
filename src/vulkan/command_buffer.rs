33676fe4ba4edcdacd07391bd7b51f2d9aa5d821
Stefnotch <brandmairstefan@gmail.com>
2023-12-25 11:09:44 +0100

    failure n

--- src/vulkan/command_buffer.rs
+++ src/vulkan/command_buffer.rs
@@ -4,7 +4,7 @@ pub use commands::*;
 pub use sync_commands::*;
 
 use std::{
-    cell::{Ref, RefCell, RefMut},
+    cell::{Ref, RefCell},
     collections::HashMap,
     sync::{atomic::AtomicU64, Arc},
 };
@@ -13,12 +13,116 @@ use ash::vk::{self};
 
 use super::{command_pool::CommandPool, context::Context, sync_manager::SyncManagerLock};
 
-struct ResourceHolder {
+struct ConsListEntry<T> {
+    value: T,
+    next: Option<Box<ConsListEntry<T>>>,
+}
+
+impl<T> ConsListEntry<T> {
+    pub fn new(value: T) -> Self {
+        Self { value, next: None }
+    }
+
+    pub fn prepend(self, value: T) -> Self {
+        Self {
+            value,
+            next: Some(Box::new(self)),
+        }
+    }
+}
+
+struct ResourceHolder2<T> {
+    resources: Option<ConsListEntry<T>>,
+}
+
+impl<T> ResourceHolder2<T> {
+    pub fn new() -> Self {
+        Self { resources: None }
+    }
+
+    pub fn add_resource(&mut self, resource: T) {
+        match self.resources.take() {
+            None => {
+                self.resources = Some(ConsListEntry::new(resource));
+            }
+            Some(entry) => {
+                self.resources = Some(entry.prepend(resource));
+            }
+        }
+    }
+
+    pub fn get_resource(&self) -> Option<&T> {
+        self.resources.as_ref().map(|entry| &entry.value)
+    }
+
+    pub fn get_resource_mut(&mut self) -> Option<&mut T> {
+        self.resources.as_mut().map(|entry| &mut entry.value)
+    }
+}
+
+pub struct ResourceHolder {
+    resources: Vec<Arc<dyn std::any::Any>>,
+}
+
+struct ArcWithLifetime<'a, T: 'a> {
+    arc: Arc<T>,
+    _lifetime: std::marker::PhantomData<&'a T>,
+}
+impl<'a, T: 'a> ArcWithLifetime<'a, T> {
+    pub fn new(arc: Arc<T>) -> Self {
+        Self {
+            arc,
+            _lifetime: std::marker::PhantomData,
+        }
+    }
+    pub fn into_ref(self) -> &'a T {
+        todo!()
+        //self.arc.as_ref()
+    }
+}
+
+impl<'a, T: 'a> Clone for ArcWithLifetime<'a, T> {
+    fn clone(&self) -> Self {
+        Self {
+            arc: self.arc.clone(),
+            _lifetime: std::marker::PhantomData,
+        }
+    }
+}
+impl<'a, T: 'a> std::ops::Deref for ArcWithLifetime<'a, T> {
+    type Target = T;
+
+    fn deref(&self) -> &Self::Target {
+        &self.arc
+    }
+}
+
+impl ResourceHolder {
+    pub fn new() -> Self {
+        Self {
+            resources: Vec::new(),
+        }
+    }
+
+    pub fn add_resource<'a, 'b, T: 'static>(&'a mut self, resource: T) -> &'b T
+    where
+        'b: 'a,
+        'a: 'b,
+    {
+        let resource = ArcWithLifetime::new(Arc::new(resource));
+        self.resources.push(resource.arc.clone());
+        let d: &'b Arc<_> = self.resources.last().unwrap();
+        let d: &'b Arc<T> = d.as_ref().downcast_ref().unwrap();
+        d
+    }
+}
+
+struct ResourceHolder3 {
     resources: RefCell<HashMap<u64, Box<dyn std::any::Any>>>,
     counter: AtomicU64,
 }
 
-impl ResourceHolder {
+impl ResourceHolder3 {
     pub fn new() -> Self {
         Self {
             resources: RefCell::new(HashMap::new()),
@@ -39,16 +143,31 @@ impl ResourceHolder {
     }
 }
 
+
+struct x<'a>: 'a{
+    phantom 'a
+   }
+   
+   fn add(&'b mut self, v: T) -> &'a T {
+   }
+   
+   How about:
+   fn add(&'b self, v:T) -> (&self, index){
+     use interior mutability to insert
+     done with interior mut
+     return (self, index)
+   }
+
 #[test]
 fn testo() {
-    let holder = ResourceHolder::new();
+    let mut holder = ResourceHolder::new();
     let res = vec![1, 2, 3];
     let res = holder.add_resource(res);
-    println!("{:?}", res);
+    println!("{:?}", *res);
     let res_2 = holder.add_resource(vec![1, 2, 3]);
-    println!("{:?}", res_2);
+    println!("{:?}", *res_2);
     let res_3 = holder.add_resource(vec![1, 2, 3]);
-    println!("{:?}", res_3);
+    println!("{:?}", *res_3);
     println!("{:?} {:?} {:?}", res[0], res_2[2], res_3[1]);
 }
 
