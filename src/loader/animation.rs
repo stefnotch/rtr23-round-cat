0993ba49d0a90b4fd10800ccb682bab26631153a
Stefnotch <brandmairstefan@gmail.com>
2024-01-09 16:44:18 +0100

    Actually lerp when playing animations

--- src/loader/animation.rs
+++ src/loader/animation.rs
@@ -1,4 +1,4 @@
-use ultraviolet::{Rotor3, Vec3};
+use ultraviolet::{Lerp, Rotor3, Vec3};
 
 use crate::transform::Transform;
 
@@ -41,21 +41,32 @@ impl Animation {
         AnimationKeyframe(keyframe_index.unwrap_or_default())
     }
 
-    pub fn sample(&self, keyframe: AnimationKeyframe, _timestamp: f32) -> Transform {
-        // TODO: Interpolation
-        let position = self.translations.get(keyframe.0);
-        let orientation = self.rotations.get(keyframe.0);
+    pub fn sample(&self, keyframe: AnimationKeyframe, timestamp: f32) -> Transform {
+        if self.timestamps.get(keyframe.0).is_none() {
+            return Default::default();
+        }
 
-        match (position, orientation) {
-            (Some(position), Some(orientation)) => Transform {
-                position: *position,
-                orientation: *orientation,
-                ..Default::default()
-            },
-            _ => Default::default(),
+        let position = get_and_next(&self.translations, keyframe.0, || Vec3::zero());
+        let orientation = get_and_next(&self.rotations, keyframe.0, Rotor3::identity);
+        let t = get_and_next(&self.timestamps, keyframe.0, || 0.0);
+
+        let t = (timestamp - t.0) / (t.1 - t.0).max(0.0001);
+        Transform {
+            position: position.0.lerp(position.1, t),
+            orientation: orientation.0.lerp(orientation.1, t),
+            ..Default::default()
         }
     }
 }
 
+fn get_and_next<T: Copy>(values: &Vec<T>, index: usize, make_default: fn() -> T) -> (T, T) {
+    let value = values.get(index).copied().unwrap_or_else(make_default);
+    let next_value = values
+        .get((index + 1).rem_euclid(values.len()))
+        .copied()
+        .unwrap_or_else(make_default);
+
+    (value, next_value)
+}
 #[derive(Default, Copy, Clone)]
 pub struct AnimationKeyframe(usize);
